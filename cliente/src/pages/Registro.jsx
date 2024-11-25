2c4c6c45ebf43606c0f48fa39f0597b6ed99c68c
felipeFleite <felipefracalossileite@gmail.com>
2024-11-25 17:32:16 -0400

    registro
    sei la ta dando errado

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Registrar() {
+  const [nome, setNome] = useState([])
+  const [email, setEmail] = useState([])
+
+    useEffect(() => {
+      const registrarDados = async () => {
+        event.preventDefault()
+        try{
+          await fetch('https://localhost:3000/usuarios'),{ 
+          method : 'POST',
+          headers: {'Content-Type' : 'application/json'},
+          body: JSON.stringify({
+            nome: nome,
+            email: email
+          })
+        })
+      } catch{
+          alert('Ocorreu erro na aplicação')
+      }
+  }
+}
 
 
   return (
-        
+    <main>
+    <form onSubmit="registrarDados()">
+
+      <input onchange="registrarDados()" type="text" value={nome}></input>
+      <input onchange="registrarDados()" type="text" value={email}></input>
+      <button></button>
+    </form>
+  </main>
   );
-}
